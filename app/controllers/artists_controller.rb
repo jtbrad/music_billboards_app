9e08590f228add50679c8041e239700b613ad40f
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-14 16:11:43 -0700

    added private method set_artist to Artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,22 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit]
 
   def index
     @artists = Artist.all
   end
 
   def show
-    @artist = Artist.find(params[:id])
+    
   end
+
+  def edit
+
+  end
+
+  private
+
+    def set_artist
+      @artist = Artist.find(params[:id])
+    end
   
 end
