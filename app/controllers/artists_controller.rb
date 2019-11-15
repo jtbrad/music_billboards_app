50d5a5be9e5ecb3a1821ee579c009140501c3d9b
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-14 17:28:48 -0700

    added create functionality to Artist class

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,13 +9,28 @@ class ArtistsController < ApplicationController
     
   end
 
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+
+    if @artist.save
+      redirect_to artists_path
+    else
+      render :new
+    end
+
+  end
+
   def edit
 
   end
 
   def update
 
-    if @artist.update(params.require(:artist).permit(:first_name, :last_name, :age, :city, :instrument))
+    if @artist.update(artist_params)
       redirect_to artist_path(@artist)
     else
       render :edit
@@ -27,6 +42,10 @@ class ArtistsController < ApplicationController
 
     def set_artist
       @artist = Artist.find(params[:id])
+    end
+
+    def artist_params
+      params.require(:artist).permit(:first_name, :last_name, :age, :city, :instrument)
     end
   
 end
