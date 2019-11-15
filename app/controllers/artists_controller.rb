f20556352fa578bece97521cb002f937d13b675e
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-14 17:36:46 -0700

    added delete functionality to Artist controller and views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,5 @@
 class ArtistsController < ApplicationController
-  before_action :set_artist, only: [:show, :edit, :update]
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
 
   def index
     @artists = Artist.all
@@ -36,6 +36,11 @@ class ArtistsController < ApplicationController
       render :edit
     end
 
+  end
+
+  def destroy
+    @artist.destroy
+    redirect_to artists_path
   end
 
   private
