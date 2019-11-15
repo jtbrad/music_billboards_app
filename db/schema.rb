6b71d2e6f2cc41746a664cf1980f9682aad64031
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-15 16:08:50 -0700

    added the Billboard model and generated new data for the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_210343) do
+ActiveRecord::Schema.define(version: 2019_11_15_225723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2019_11_15_210343) do
     t.integer "age"
     t.string "city"
     t.string "instrument"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "billboards", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-def generate_new_db
-  i = 1
+def generate_billboards
+  Billboard.create(name: "Top 10")
+  Billboard.create(name: "Top 50")
+end
 
+def generate_song
+  @artist.songs.create(
+    title: "Song #{Song.count + 1}",
+    genre: Faker::Music.genre
+  )
+end
+
+def generate_artists
   Artist.all.destroy_all
   
   10.times do
-    artist = Artist.create(
+    @artist = Artist.create(
       first_name: Faker::Name.first_name,
       last_name: Faker::Name.last_name,
       age: rand(25..50),
@@ -21,17 +31,17 @@ def generate_new_db
     )
   
     5.times do
-      artist.songs.create(
-        title: "Song #{i}",
-        genre: Faker::Music.genre
-      )
-      i += 1
+      generate_song
     end
     
   end
 
-  puts "New data entered."
+end
 
+def generate_new_db
+  generate_billboards
+  generate_artists
+  puts "New data entered."
 end
 
 generate_new_db
